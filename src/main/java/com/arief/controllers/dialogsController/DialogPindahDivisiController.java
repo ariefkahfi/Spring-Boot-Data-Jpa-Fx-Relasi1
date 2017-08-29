761b1301cf6a6dd0ece1953b99c565ac7f82e46f
Arief Kahfi <arief@gmail.com>
2017-08-29 16:23:40 +0700

    Update DialogPindahDivisi dan DialogUbahJabatan

---
+++ src/main/java/com/arief/controllers/dialogsController/DialogPindahDivisiController.java
@@ -0,0 +1,7 @@
+package com.arief.controllers.dialogsController;
+
+/**
+ * Created by Arief on 8/29/2017.
+ */
+public class DialogPindahDivisiController {
+}
---
+++ src/main/java/com/arief/services/FxServices/FxServiceDatabaseTransactionForKaryawan.java
@@ -0,0 +1,7 @@
+package com.arief.services.FxServices;
+
+/**
+ * Created by Arief on 8/29/2017.
+ */
+public class FxServiceDatabaseTransactionForKaryawan {
+}
